65362f9ef4881744f4db94557cd87ba0f56ea18a
hans <68971752+hanlin487@users.noreply.github.com>
2023-05-12 04:03:34 -0700

    Add files via upload

---
+++ apps/queue_tester.c
@@ -0,0 +1,146 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <queue.h>
+
+#define TEST_ASSERT(assert)				\
+do {									\
+	printf("ASSERT: " #assert " ... ");	\
+	if (assert) {						\
+		printf("PASS\n");				\
+	} else	{							\
+		printf("FAIL\n");				\
+		exit(1);						\
+	}									\
+} while(0)
+
+/* Create */
+void test_create(void)
+{
+	fprintf(stderr, "\n*** TEST create ***\n");
+
+	TEST_ASSERT(queue_create() != NULL);
+}
+
+/* Enqueue/Dequeue simple */
+void test_queue_simple(void)
+{
+	int data = 3, *ptr;
+	queue_t q;
+
+	fprintf(stderr, "\n*** TEST queue_simple ***\n");
+
+	q = queue_create();
+	queue_enqueue(q, &data);
+	queue_dequeue(q, (void**)&ptr);
+	TEST_ASSERT(ptr == &data);
+}
+
+static void iterator_inc(queue_t q, void *data)
+{
+    int *a = (int*)data;
+
+    if (*a == 42)
+        queue_delete(q, data);
+    else
+        *a += 1;
+}
+
+void test_iterator_del()
+{
+    fprintf(stderr, "\n*** TEST iterator_del ***\n");
+    queue_t q;
+    int data[] = {1, 2, 3, 4, 5, 42, 6, 7, 8, 9};
+    size_t i;
+
+    /* Initialize the queue and enqueue items */
+    q = queue_create();
+    for (i = 0; i < sizeof(data) / sizeof(data[0]); i++)
+        queue_enqueue(q, &data[i]);
+
+    /* Increment every item of the queue, delete item '42' */
+    queue_iterate(q, iterator_inc);
+    TEST_ASSERT(data[0] == 2);
+    TEST_ASSERT(queue_length(q) == 9);
+}
+
+void test_destroy_empty(){
+    fprintf(stderr, "\n*** TEST destroy_empty ***\n");
+    queue_t q = queue_create();
+    TEST_ASSERT(queue_destroy(q) == 0);
+}
+
+void test_delete_empty(){
+    fprintf(stderr, "\n*** TEST delete_empty ***\n");
+    queue_t q = queue_create();
+    int data = 10;
+    void *ptr = &data;
+    TEST_ASSERT(queue_delete(q, ptr) == -1);
+}
+
+void test_dequeue_empty(){
+    fprintf(stderr, "\n*** TEST dequeue_empty ***\n");
+    queue_t q = queue_create();
+    int *ptr;
+    TEST_ASSERT(queue_dequeue(q, (void**) &ptr) == -1);
+}
+
+void test_delete_same(){
+    fprintf(stderr, "\n*** TEST delete_same ***\n");
+    queue_t q = queue_create();
+    int data = 10;
+    TEST_ASSERT(queue_enqueue(q, &data) == 0);
+    TEST_ASSERT(queue_length(q) == 1);
+    TEST_ASSERT(queue_delete(q, &data) == 0);
+    TEST_ASSERT(queue_delete(q, &data) == -1);
+}
+
+void test_destroy_nonempty(){
+    fprintf(stderr, "\n*** TEST destroy_nonempty ***\n");
+    queue_t q = queue_create();
+    int data = 1001;
+    queue_enqueue(q, &data);
+    TEST_ASSERT(queue_destroy(q) == -1);
+}
+
+void test_del_head_tail(){
+    fprintf(stderr, "\n*** TEST del_head_tail ***\n");
+    queue_t q = queue_create();
+    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, *ptr;
+    size_t i;
+
+    for (i = 0; i <sizeof(data) / sizeof(data[0]); i++){
+        queue_enqueue(q, &data[i]);
+    }
+
+    TEST_ASSERT(queue_length(q) == 10);
+    queue_delete(q, &data[0]);
+    queue_delete(q, &data[9]);
+    TEST_ASSERT(queue_length(q) == 8);
+    queue_dequeue(q, (void**) &ptr);
+    TEST_ASSERT(*ptr == 2);
+    queue_dequeue(q, (void**) &ptr); /* ptr = 3*/
+    queue_dequeue(q, (void**) &ptr); /* ptr = 4*/
+    queue_dequeue(q, (void**) &ptr); /* ptr = 5*/
+    queue_dequeue(q, (void**) &ptr); /* ptr = 6*/
+    queue_dequeue(q, (void**) &ptr); /* ptr = 7*/
+    queue_dequeue(q, (void**) &ptr); /* ptr = 8*/
+    queue_dequeue(q, (void**) &ptr); /* ptr = 9*/
+    TEST_ASSERT(*ptr == 9);
+    TEST_ASSERT(queue_length(q) == 0);
+}
+
+int main(void){
+    test_create();
+    test_queue_simple();
+    test_iterator_del();
+    test_destroy_empty();
+    test_delete_empty();
+    test_dequeue_empty();
+    test_delete_same();
+    test_destroy_nonempty();
+    test_del_head_tail();
+    return 0;
+}
